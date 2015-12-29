1aa77503a7dd3a1c7a0417b595fa7519232b44c3
zach pratt <zach@attackofzach.com>
2015-12-28 21:25:39 -0600

    update the url with the search text

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {syncReduxAndRouter} from 'redux-simple-router';
+import {createHistory} from 'history'
 
 import {create as createStore} from './lib/store-factory';
 import ResultsProvider from './lib/results-provider';
 
-const store = createStore();
+const store = createStore(),
+    history = createHistory();
 
-ReactDOM.render(<ResultsProvider store={store} />, document.querySelector('.app'));
+syncReduxAndRouter(history, store);
+
+ReactDOM.render((
+    <ResultsProvider
+        history={history}
+        store={store}
+    />
+), document.querySelector('.app'));
